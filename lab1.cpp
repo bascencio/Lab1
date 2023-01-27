fb56d993aa1489a57eca2597ceb945c69abbc793
Steam Deck User <bascencio@csub.edugit config -- global user.name Brian>
2023-01-27 13:16:10 -0800

    my first commit for lab-1

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+//Brian Ascencio
+//lab-1
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+   // This is the newwest change 
+   // used for later
+   int a = rand() % 10 + 1;
+   cout << a << endl;
+   cout << "Hello Word\n";
+   
+    return 0;
+}
+
+
